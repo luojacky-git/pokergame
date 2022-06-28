e2c684164cd42e7dbff71c36a5fdf377cb774269
luojacky-git <luojacky99@gmail.com>
2022-06-27 21:45:42 -0400

    Initial working setup

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const port = process.env.PORT || 30000;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localsConvention: "camelCase",
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+  },
+};
